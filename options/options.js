3351b90015e6f1b95e2e1de90924b086cc764ae2
Crowdbotics <engineering@crowdbotics.com>
2023-01-22 00:07:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curly-credit-dev-58497",
+  url: "https://curly-credit-dev-58497.botics.co",
+  api: "https://curly-credit-dev-58497.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
